a275c6a5afa0cae5c0a20e714e1cd5b32a0b0e83
Enrico Deleo <hello@enricodeleo.com>
2019-09-26 02:26:36 +0200

    feature: cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const hackerQuotes = require('hacker-quotes');
+const cors = require('micro-cors')();
 
-module.exports = () => {
+module.exports = cors(() => {
   const min = Math.ceil(0);
   const max = Math.floor(hackerQuotes.length);
   const index = Math.floor(Math.random() * (max - min)) + min;
 
-  return hackerQuotes[index];
-};
+  return {
+    quote: hackerQuotes[index].quote || hackerQuotes[index].qoute,
+    author: hackerQuotes[index].cite,
+  };
+});
