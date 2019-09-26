440f4ab03efc963b2eb3da20dbc73d2ac5cc9e81
Enrico Deleo <hello@enricodeleo.com>
2019-09-26 02:09:42 +0200

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const hackerQuotes = require('hacker-quotes');
+
+module.exports = () => {
+  const min = Math.ceil(0);
+  const max = Math.floor(hackerQuotes.length);
+  const index = Math.floor(Math.random() * (max - min)) + min;
+
+  return hackerQuotes[index].quote;
+};
